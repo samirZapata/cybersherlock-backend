b0505bfd4dafeffe00b069f473bebc7e2170fb21
Yonier Zapata Asprilla <samirzapata11@gmail.com>
2023-10-25 23:26:05 -0500

    Fixing production error

--- build/index.js
+++ build/index.js
@@ -2,6 +2,6 @@
 var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var app = (0, _express["default"])();
-const port = env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 console.log('Server listen on port', port);
