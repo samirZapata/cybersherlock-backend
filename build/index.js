4aed8d1cfd324b71157de6f2176da0080cb7aed9
Yonier Zapata Asprilla <samirzapata11@gmail.com>
2023-10-25 23:20:18 -0500

    Fixed Build's error in production

--- build/index.js
+++ build/index.js
@@ -1,8 +1,7 @@
 "use strict";
-import app from './app';
 var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var app = (0, _express["default"])();
-const port = app.get('port');
+const port = env.PORT || 3000;
 app.listen(port);
-console.log('Server listen on port 3000');
+console.log('Server listen on port', port);
--- src/database.js
+++ src/database.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://admin:BsGmmhgC_kK59wE@atlascluster.v91zgqv.mongodb.net/?retryWrites=true&w=majority",{
+export const con =  mongoose.connect("mongodb+srv://admin:BsGmmhgC_kK59wE@atlascluster.v91zgqv.mongodb.net/?retryWrites=true&w=majority",{
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
 .then(db => console.log("DB is connected"))
 .catch(err => console.log(err));
+
+if(con){
+    console.log("DB is connected");
+}else{
+    console.log("DB is not connected");
+}
