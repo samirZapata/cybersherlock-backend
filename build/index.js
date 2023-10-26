49862d8750be115b4f76d46ec39799f49ca4d455
Yonier Zapata Asprilla <samirzapata11@gmail.com>
2023-10-25 22:44:33 -0500

    Fixed errors

--- build/index.js
+++ build/index.js
@@ -1,7 +1,8 @@
 "use strict";
-
+import app from './app';
 var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 var app = (0, _express["default"])();
-app.listen(3000);
+const port = app.get('port');
+app.listen(port);
 console.log('Server listen on port 3000');
