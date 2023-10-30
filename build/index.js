b7e3a8c6fb6aa645c44624f79f965f6fce1694de
Yonier Zapata Asprilla <samirzapata11@gmail.com>
2023-10-30 12:34:04 -0500

    Errors fixed

--- build/index.js
+++ build/index.js
@@ -1,7 +1,3 @@
 "use strict";
 var _express = _interopRequireDefault(require("express"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var app = (0, _express["default"])();
-const port = process.env.PORT || 9000;
-app.listen(port);
-console.log('Server listen on port', port);
